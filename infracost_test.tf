93e6124d57dd7b3a6e8a68c9e703819f4a0c7c56
Shiak Khalifa B <shiakhussiankhalifa@gmail.com>
2025-12-05 00:13:28 +0530

    Update infracost_test.tf

--- infracost_test.tf
+++ infracost_test.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 6.2.0, < 7.0.0"
-    }
-  }
-  required_version = ">= 1.4.0"
-}
-
-
-provider "aws" {
-  region = "ap-south-1"
-}
-
-
 locals{
   pairs ={
      for index, value in var.subnet: index =>{
